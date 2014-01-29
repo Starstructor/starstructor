8a78a61e6e4901caa09d349baf7b68064390b041
cstamford <cstamford@gmail.com>
2014-01-29 03:00:30 +0000

    header update, change max width / max height

--- Starstructor/GUI/ResizePart.cs
+++ Starstructor/GUI/ResizePart.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿/*Starstructor, the Starbound Toolet 
+Copyright (C) 2013-2014 Chris Stamford
+Contact: cstamford@gmail.com
+
+Source file contributers:
+ Chris Stamford     contact: cstamford@gmail.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+using System;
 using System.Windows.Forms;
 using Starstructor.Data;
 
@@ -21,8 +37,8 @@ namespace Starstructor.GUI
 
         private const int MIN_WIDTH = 1;
         private const int MIN_HEIGHT = 1;
-        private const int MAX_WIDTH = 1024;
-        private const int MAX_HEIGHT = 1024;
+        private const int MAX_WIDTH = 512;
+        private const int MAX_HEIGHT = 512;
 
         public ResizePart(int baseWidth, int baseHeight)
         {
